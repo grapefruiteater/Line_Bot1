e4bb9bf0dd48f7252fa153cff362a5ae737ae367
grapefruiteater <88916563+grapefruiteater@users.noreply.github.com>
2021-11-28 20:04:39 +0900

    Update Line_Bot.py

--- Line_Bot.py
+++ Line_Bot.py
@@ -20,7 +20,10 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-
+#herokuへのデプロイが成功したかどうかを確認するためのコード
+@app.route("/")
+def hello_world():
+    return "hello world!"
 
 
 #LINE DevelopersのWebhookにURLを指定してWebhookからURLにイベントが送られるようにする
