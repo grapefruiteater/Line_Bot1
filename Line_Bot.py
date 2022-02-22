fb110a0e513c116b5fcb0d54aaa1a913ce93471c
grapefruiteater <88916563+grapefruiteater@users.noreply.github.com>
2022-02-22 22:59:42 +0900

    Update Line_Bot.py

--- Line_Bot.py
+++ Line_Bot.py
@@ -61,7 +61,7 @@ def callback():
 #以下でWebhookから送られてきたイベントをどのように処理するかを記述する
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-   tmp_text = event.message.text
+   send_message = event.message.text
    rep = talkapi(tmp_text)
    if send_message == "現在":
        line_bot_api.reply_message(
