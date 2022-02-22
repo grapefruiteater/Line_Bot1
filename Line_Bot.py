f2e912aa0b2ee7a83fadcfe55fc1f1e647ec9d4d
grapefruiteater <88916563+grapefruiteater@users.noreply.github.com>
2022-02-22 13:39:41 +0900

    Update Line_Bot.py

--- Line_Bot.py
+++ Line_Bot.py
@@ -63,7 +63,65 @@ def handle_message(event):
        event.reply_token,
        TextSendMessage(text=msg))
 
+def createRichmenu():
+    result = False
+    try:
+        # define a new richmenu
+        rich_menu_to_create = RichMenu(
+            size = RichMenuSize(width=1200, height=405),
+            selected = True,
+            name = 'richmenu for randomchat',
+            chat_bar_text = 'TAP HERE',
+            areas=[
+                RichMenuArea(
+                    bounds=RichMenuBounds(x=0, y=0, width=480, height=405),
+                    action=MessageAction(text=config.REMOVE)
+                ),
+                RichMenuArea(
+                    bounds=RichMenuBounds(x=480, y=0, width=720, height=405),
+                    action=MessageAction(text=config.NEXT)
+                )
+            ]
+        )
+        richMenuId = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
+
+        # upload an image for rich menu
+        path = 'image path for richmenu'
+
+        with open(path, 'rb') as f:
+            line_bot_api.set_rich_menu_image(richMenuId, "image/jpeg", f)
+
+        # set the default rich menu
+        line_bot_api.set_default_rich_menu(richMenuId)
+
+        result = True
+
+    except Exception:
+        result = False
+
+
+    return result
+
 # ポート番号の設定
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+    
+    # define a new richmenu
+    rich_menu_to_create = RichMenu(
+        size = RichMenuSize(width=1200, height=405),
+        selected = True,
+        name = 'richmenu for randomchat',
+        chat_bar_text = 'TAP HERE',
+        areas=[
+            RichMenuArea(
+                bounds=RichMenuBounds(x=0, y=0, width=480, height=405),
+                action=MessageAction(text=config.REMOVE)
+            ),
+            RichMenuArea(
+                bounds=RichMenuBounds(x=480, y=0, width=720, height=405),
+                action=MessageAction(text=config.NEXT)
+            )
+        ]
+    )
+    richMenuId = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
