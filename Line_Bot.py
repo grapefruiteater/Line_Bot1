00dc23a84d5880ed2dffde2e901b97d04e98e941
grapefruiteater <88916563+grapefruiteater@users.noreply.github.com>
2022-02-22 23:06:08 +0900

    Update Line_Bot.py

--- Line_Bot.py
+++ Line_Bot.py
@@ -62,7 +62,6 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     send_message = event.message.text
-    send_message == "座席表":
     line_bot_api.reply_message(
        event.reply_token,
        (ImageSendMessage(original_content_url="https://d4xawcq9u1fih.cloudfront.net/data8.png",
