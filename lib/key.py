7ad848839ca1cb31f6dc7aac7a8a5775e149d243
grapefruiteater <88916563+grapefruiteater@users.noreply.github.com>
2022-02-23 14:48:01 +0900

    Update key.py

--- lib/key.py
+++ lib/key.py
@@ -4,16 +4,16 @@ import sys
 ############################################################################
 # API_KEYを設定
 ############################################################################
-# get channel_secret and channel_access_token from your environment variable
-channel_secret = os.environ.get('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.environ.get('LINE_CHANNEL_ACCESS_TOKEN', None)
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get('YOUR_CHANNEL_ACCESS_TOKEN', None)
+YOUR_CHANNEL_SECRET = os.environ.get('YOUR_CHANNEL_SECRET', None)
+TALKAPI_KEY = os.environ.get('TALKAPI_KEY', None)
 
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+if YOUR_CHANNEL_ACCESS_TOKEN is None:
+    print('Specify YOUR_CHANNEL_ACCESS_TOKEN as environment variable.')
     sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+if YOUR_CHANNEL_SECRET is None:
+    print('Specify YOUR_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
-if google_photo_client_id is None:
-    print('Specify GOOGLE_PHOTO_CLIENT_ID as environment variable.')
+if TALKAPI_KEY is None:
+    print('Specify TALKAPI_KEY as environment variable.')
     sys.exit(1)
