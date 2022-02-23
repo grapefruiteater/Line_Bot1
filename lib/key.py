6a751be400e15d8873ad3852f2a6250490aeb72a
grapefruiteater <88916563+grapefruiteater@users.noreply.github.com>
2022-02-23 15:28:29 +0900

    Update key.py

--- lib/key.py
+++ lib/key.py
@@ -1,8 +1,9 @@
 import os
 import sys
 from os.path import join, dirname
-
+from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 ############################################################################
 # API_KEYを設定
