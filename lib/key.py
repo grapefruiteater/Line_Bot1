cb84227bea2f70c6bc36932f2f7e384b7f197990
grapefruiteater <88916563+grapefruiteater@users.noreply.github.com>
2022-02-23 16:45:59 +0900

    Update key.py

--- lib/key.py
+++ lib/key.py
@@ -11,6 +11,10 @@ load_dotenv(dotenv_path)
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get('YOUR_CHANNEL_ACCESS_TOKEN', None)
 YOUR_CHANNEL_SECRET = os.environ.get('YOUR_CHANNEL_SECRET', None)
 TALKAPI_KEY = os.environ.get('TALKAPI_KEY', None)
+google_photo_client_id = os.environ.get('google_photo_client_id', None)
+google_photo_client_secret = os.environ.get('google_photo_client_secret', None)
+google_photo_album_id = os.environ.get('google_photo_album_id', None)
+google_photo_refresh_token = os.environ.get('google_photo_refresh_token', None)
 
 if YOUR_CHANNEL_ACCESS_TOKEN is None:
     print('Specify YOUR_CHANNEL_ACCESS_TOKEN as environment variable.')
@@ -20,4 +24,16 @@ if YOUR_CHANNEL_SECRET is None:
     sys.exit(1)
 if TALKAPI_KEY is None:
     print('Specify TALKAPI_KEY as environment variable.')
+    sys.exit(1)
+if google_photo_client_id is None:
+    print('Specify GOOGLE_PHOTO_CLIENT_ID as environment variable.')
+    sys.exit(1)
+if google_photo_client_secret is None:
+    print('Specify GOOGLE_PHOTO_CLIENT_SECRET as environment variable.')
+    sys.exit(1)
+if google_photo_album_id is None:
+    print('Specify GOOGLE_PHOTO_ALBUM_ID as environment variable.')
+    sys.exit(1)
+if google_photo_refresh_token is None:
+    print('Specify GOOGLE_PHOTO_REFRESH_TOKEN as environment variable.')
     sys.exit(1)
